a753ad8f1599a8b0151a1276864fe76806a9bb74
Gabriel Girardi <gabrielgirardi2811@gmail.com>
2022-11-16 22:17:02 -0300

    chore: ajustado menu para desktop

--- js/main.js
+++ js/main.js
@@ -6,13 +6,42 @@ window.onload = function() {
 
     menuBtn.addEventListener("click", function() {
         menuOptions.classList.toggle("active");
+        menuBtn.firstElementChild.classList.toggle("menu-opened")
         logo.classList.toggle("hide");
         menuBtn.classList.toggle("opened");
+
         
         if(menuOptions.classList.contains("active")) {
-            menuBtn.firstElementChild.setAttribute('src', './assets/close-hambur.png')
+            menuBtn.firstElementChild.setAttribute('src', './assets/arrow-back.svg');
         } else {
-            menuBtn.firstElementChild.setAttribute('src', './assets/menu-hambur.png')
+            menuBtn.firstElementChild.setAttribute('src', './assets/menu-hambur.svg')
         }
     });
+
+    let nomeBtn = document.querySelector('.name')
+    let textWelcome = document.querySelector('.welcome-text');
+
+
+    nomeBtn.addEventListener("click", function() {
+        let nome = prompt('Qual o seu nome?');
+
+        let nomeRegistered = {nome}
+        let nomeString = localStorage.getItem('nome');
+        let nomeObj = JSON.parse(nomeString);
+
+
+        localStorage.setItem('nome', JSON.stringify(nomeRegistered));
+        nomeBtn.textContent = (nomeObj.nome);
+    });
+
+    setInterval(function () {
+        let nomeString = localStorage.getItem('nome');
+        let nomeObj = JSON.parse(nomeString);
+
+        nomeBtn.textContent = (nomeObj.nome);
+
+        if(nomeBtn = nomeObj) {
+            textWelcome.textContent = ('Olá! ');
+        }
+    }, 300);
 };
